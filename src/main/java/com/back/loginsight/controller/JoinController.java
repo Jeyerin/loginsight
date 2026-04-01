4a807e5f2517e98a8b691e1003a02af9ea1f9685
yerin <wiiow9999@gmail.com>
2026-04-02 01:47:59 +0900

    - swagger 기본 어노테이션 적용

--- src/main/java/com/back/loginsight/controller/JoinController.java
+++ src/main/java/com/back/loginsight/controller/JoinController.java
@@ -1,15 +1,19 @@
 package com.back.loginsight.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/join")
 public class JoinController {
 
-    @GetMapping("/hello")
-    public String hello() {
+    @Operation(summary = "회원가입", description = "회원가입을 합니다.")
+    @GetMapping("/reg")
+    public String signUp(
+            @Parameter(description = "이름",example = "홍길동",required = true) String name) {
         return "hello";
     }
 }
